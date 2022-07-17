690b590961a1c4644ed365519ecaaf4059a0fdd8
Cory Finger <cory.j.finger@gmail.com>
2022-07-16 23:09:35 -0700

    Have two LEDs blink on a different schedule

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use core::panic::PanicInfo;
 use arduino_nano33iot as bsp;
 use bsp::hal;
 use hal::clock::GenericClockController;
-use hal::delay::Delay;
 use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
 use bsp::hal::gpio;
@@ -16,16 +15,14 @@ use hal::pac::{interrupt};
 struct Machine {
     led: gpio::v2::Pin<gpio::v2::PA17, gpio::v2::Output<gpio::v2::PushPull>>,
     ext_led: gpio::v2::Pin<gpio::v2::PA21, gpio::v2::Output<gpio::v2::PushPull>>,
-    delay: Delay,
-    nvic: NVIC
+    tc3: hal::timer::TimerCounter3,
+    tc4: hal::timer::TimerCounter4
 }
-
 
 impl Machine {
     fn new() -> Self {
         let mut peripherals = Peripherals::take().unwrap();
-        let core = CorePeripherals::take().unwrap();
-        let nvic = core.NVIC;
+        let mut core = CorePeripherals::take().unwrap();
         
         let mut clocks = GenericClockController::with_internal_32kosc(
             peripherals.GCLK,   
@@ -35,20 +32,41 @@ impl Machine {
         );
         let pins = bsp::Pins::new(peripherals.PORT);
         let led: bsp::Led = pins.led_sck.into();
-        let ext_led = pins.d10.into_push_pull_output();
-        let delay = Delay::new(core.SYST, &mut clocks);
+        let ext_led = pins.d10.into();
+
+        let gclk0 = clocks.gclk0();
+        let tc3 = hal::timer::TimerCounter::tc3_(
+            &clocks.tcc2_tc3(&gclk0).unwrap(),
+            peripherals.TC3,
+            &mut peripherals.PM,
+        );
+
+        let tc4 = hal::timer::TimerCounter::tc4_(
+            &clocks.tc4_tc5(&gclk0).unwrap(),
+            peripherals.TC4,
+            &mut peripherals.PM,
+        );
+
+        unsafe {
+            // Enable the interrupt line for the timer
+            NVIC::unmask(hal::pac::Interrupt::TC3);
+            NVIC::unmask(hal::pac::Interrupt::TC4);
+
+            // Set priority for the timer interrupt
+            core.NVIC.set_priority(hal::pac::Interrupt::TC3, 1);
+            core.NVIC.set_priority(hal::pac::Interrupt::TC4, 2);
+        }
 
         Machine{
             led,
             ext_led,
-            delay,
-            nvic
+            tc3,
+            tc4
         }
     }
 }
 
 static mut MACHINE: Option<Machine> = None;
-static mut INTERRUPT_COUNT: u32 = 0;
 
 #[entry]
 fn main() -> ! {
@@ -57,31 +75,37 @@ fn main() -> ! {
         MACHINE.as_mut().unwrap()
     };
 
-    unsafe {
-        NVIC::unmask(hal::pac::Interrupt::I2S);
-        machine.nvic.set_priority(hal::pac::Interrupt::I2S, 1 << 0);
-    }
+    cortex_m::interrupt::free(|_cs| {
+        machine.led.set_low().unwrap();
+    });
+
+    machine.tc3.start(2.hz());
+    machine.tc3.enable_interrupt();
+
+    machine.tc4.start(10.hz());
+    machine.tc4.enable_interrupt();
 
     loop {
-        machine.delay.delay_ms(500u16);
-
-        // Fire I2S() and then come back to this loop.
-        NVIC::pend(hal::pac::Interrupt::I2S);
+        cortex_m::asm::wfi();
     }
 }
 
 #[interrupt]
-fn I2S() {
+fn TC3() {
     let machine = unsafe { MACHINE.as_mut().unwrap() };
-    let i = unsafe { INTERRUPT_COUNT };
+    machine.led.toggle().unwrap();
 
-    if i % 5 == 0 {
-        machine.led.set_high().unwrap();
-    } else {
-        machine.led.set_low().unwrap();
-    }
+    // Unset the timer so it can fire again
+    machine.tc3.wait().unwrap();
+}
 
-    unsafe { INTERRUPT_COUNT += 1;}
+#[interrupt]
+fn TC4() {
+    let machine = unsafe { MACHINE.as_mut().unwrap() };
+    machine.ext_led.toggle().unwrap();
+
+    // Unset the timer so it can fire again
+    machine.tc4.wait().unwrap();
 }
 
 
